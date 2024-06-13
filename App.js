b73178a331c285f24b3c905e279dea4f283b47f7
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-06-13 19:17:35 +0700

    chore: Add initial project files and dependencies

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import WeatherSearch from './src/components/weatherSearch';
+import WeatherInfo from './src/components/weatherInfo';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <WeatherSearch />
+      <WeatherInfo />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
+
+export default App;
