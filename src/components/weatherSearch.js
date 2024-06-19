b5b8247039362adf29cad4c4bec739cad8aa782d
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-06-19 20:36:12 +0700

    refactor: Remove unnecessary comments and improve code readability in WeatherSearch component

--- src/components/weatherSearch.js
+++ src/components/weatherSearch.js
@@ -10,12 +10,10 @@ const WeatherSearch = ({ searchWeather }) => {
       <CustomTextInput
         placeholder="Search the weather of your city"
         numberOfLines={1}
-        // Tambahkan text dan onChange
         text={location}
         onChange={setLocation}
       />
       <View style={styles.buttonWrapper}>
-        {/* Jalankan function searchWeather dengan parameter location */}
         <Button title="Search" onPress={() => searchWeather(location)} />
       </View>
     </View>
